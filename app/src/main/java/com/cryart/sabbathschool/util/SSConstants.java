5a69d0d83bf73e47a3617d4d831f624636672f35
Vitaliy Lim <lim.vitaliy@gmail.com>
2015-02-21 01:03:28 -0500

    Add SSConstants class to store various constants that are accessed throughout the whole app

---
+++ app/src/main/java/com/cryart/sabbathschool/util/SSConstants.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2015 Vitaliy Lim <lim.vitaliy@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.cryart.sabbathschool.util;
+
+public class SSConstants {
+    public static final String SS_SETTINGS_FALLBACK_LANGUAGE = "en";
+    public static final String SS_SETTINGS_LANGUAGE_KEY = "ss_settings_lesson_language";
+
+    public static final String SS_SETTINGS_TEXT_SIZE_KEY = "ss_settings_text_size";
+    public static final String SS_SETTINGS_TEXT_SIZE_DEFAULT_VALUE = "small.css";
+
+    public static final String SS_SETTINGS_KEN_BURNS_EFFECT_KEY = "ss_settings_ken_burns_effect";
+    public static final boolean SS_SETTINGS_KEN_BURNS_EFFECT_DEFAULT_VALUE = true;
+
+    public static final String SS_BIBLE_VERSE_ACTIVITY_ARGUMENT = "ss_day_bible_verses";
+
+    public static final int SS_PAGER_OFFSCREEN_PAGE_LIMIT = 7;
+    public static final String SS_WEBAPP_PATH_PREFIX = "file:///android_asset/sabbath-school-js/";
+    public static final String SS_WEBAPP_PLATFORM = "android";
+    public static final String SS_WEBAPP_MAIN = "sabbath-school-js/app.html";
+    public static final String SS_WEBAPP_BIBLE = "sabbath-school-js/bible.html";
+    public static final String SS_DAY_DATE_ARG = "_SSDayDate";
+    public static final String SS_POSITION = "_SSPosition";
+    public static final int TOPBAR_PADDING = 248;
+
+    public static final String SS_WEBVIEW_CLIPBOARD_LABEL = "ss_clipboard_label";
+    public static final String SS_WEBVIEW_SEARCH_PROVIDER = "https://www.google.com/search?q=%s";
+
+    public static final String SS_MATERIAL_ICON_FONT_PATH = "fonts/material-icon-font.ttf";
+    public static final String SS_FONTAWESOME_FONT_PATH = "fonts/fontawesome-webfont.ttf";
+
+    public static final String SS_ABOUT_INSTAGRAM_URL = "http://instagram.com/p/pfHeT4Nqi9";
+    public static final String SS_ABOUT_INSTAGRAM_FALLBACK_URL = "http://instagram.com/v1talique";
+    public static final String SS_ABOUT_PHOTO_CREDITS_URL = "http://instagram.com/asimsness";
+    public static final long SS_ABOUT_TWITTER_ID = 116854782L;
+    public static final String SS_ABOUT_TWITTER_FALLBACK_URL = "https://twitter.com/vitalik_l";
+}
