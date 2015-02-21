03c504bf319be29083cf5c2f0c774ffa7223d426
Vitaliy Lim <lim.vitaliy@gmail.com>
2015-02-21 01:11:34 -0500

    Add highlight, comment and all interactions with the webapp served form the assets throught javascript interface

--- app/src/main/java/com/cryart/sabbathschool/ui/fragment/SSWebViewFragment.java
+++ app/src/main/java/com/cryart/sabbathschool/ui/fragment/SSWebViewFragment.java
@@ -23,12 +23,17 @@
 package com.cryart.sabbathschool.ui.fragment;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -36,57 +41,79 @@ import android.view.ViewGroup;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.cryart.sabbathschool.R;
+import com.cryart.sabbathschool.adapters.SSTabsAdapter;
+import com.cryart.sabbathschool.model.SSDay;
+import com.cryart.sabbathschool.ui.activity.SSBibleVerseActivity;
 import com.cryart.sabbathschool.ui.activity.SSMainActivity;
-import com.cryart.sabbathschool.ui.widget.SSWebView;
 import com.cryart.sabbathschool.ui.widget.SSSlidingTabLayout;
+import com.cryart.sabbathschool.ui.widget.SSWebView;
+import com.cryart.sabbathschool.util.SSConstants;
+import com.cryart.sabbathschool.util.SSCore;
 import com.cryart.sabbathschool.util.SSHelper;
+import com.flaviofaria.kenburnsview.KenBurnsView;
 
-import org.json.JSONObject;
+public class SSWebViewFragment extends Fragment implements SSWebView.OnScrollChangedCallback {
+    private SharedPreferences _SSPreferences;
 
-public class SSWebViewFragment extends Fragment {
-    private static final String ARG_POSITION = "position";
-    private static final int TOPBAR_PADDING = 215;
+    private String _SSDayDate;
+    private SSDay _SSDay;
+    private SSCore _SSCore;
+    private int _SSPosition;
 
-    private ViewGroup _view;
     private SSWebView _SSWebView;
-
-    private int position;
     private Toolbar _SSToolbar;
     private SSSlidingTabLayout _SSTabs;
-    private ImageView _SSHero;
+    private KenBurnsView _SSHero;
     private View _SSStatusBar;
     private ProgressBar _SSWebViewLoading;
 
-
-    public static SSWebViewFragment newInstance(int position) {
+    public static SSWebViewFragment newInstance(int _SSPosition, String _SSDayDate) {
         SSWebViewFragment f = new SSWebViewFragment();
         Bundle b = new Bundle();
-        b.putInt(ARG_POSITION, position);
+        b.putInt(SSConstants.SS_POSITION, _SSPosition);
+        b.putString(SSConstants.SS_DAY_DATE_ARG, _SSDayDate);
         f.setArguments(b);
         return f;
+    }
+
+    public void loadDay(String _SSDayDate){
+        _SSWebView.setVisibility(View.INVISIBLE);
+        _SSWebViewLoading.setVisibility(View.VISIBLE);
+        _SSDay = _SSCore.ssGetDay(_SSDayDate);
+        _SSHero.setImageBitmap(SSHelper.getBitmapFromBase64(_SSDay._lesson_image));
+        ((SSMainActivity)getActivity()).setHeroView();
+
+        _SSWebView.loadDataWithBaseURL(SSConstants.SS_WEBAPP_PATH_PREFIX,
+                SSHelper.readFileFromAssets(getActivity(), SSConstants.SS_WEBAPP_MAIN)
+                    .replaceAll("\\{\\{platform\\}\\}", SSConstants.SS_WEBAPP_PLATFORM)
+                    .replaceAll("small.css", _SSPreferences.getString(SSConstants.SS_SETTINGS_TEXT_SIZE_KEY, SSConstants.SS_SETTINGS_TEXT_SIZE_DEFAULT_VALUE))
+                    .replaceAll("<div class=\"wrapper\">", "<div class=\"wrapper\" style=\"margin-top: " + (SSConstants.TOPBAR_PADDING - SSHelper.convertPixelsToDp(getActivity(), SSHelper.getStatusBarHeight(getActivity()))) + "px\">" + _SSDay._day_text),
+                "text/html", "utf-8", null);
+    }
+
+    protected void setHighlights(String highlights){
+        _SSWebView.loadUrl("javascript:ss.setHighlight('"+highlights+"');");
+    }
+
+    protected void setComments(String comments){
+        _SSWebView.loadUrl("javascript:ss.setComments('"+comments+"');");
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        position = getArguments().getInt(ARG_POSITION);
-        Log.v("onCreate", String.valueOf(position));
-        if (savedInstanceState != null){
-            Log.v("onCreate from bundle", String.valueOf(position));
-        }
+        _SSPosition = getArguments().getInt(SSConstants.SS_POSITION);
+        _SSDayDate = getArguments().getString(SSConstants.SS_DAY_DATE_ARG);
+        _SSCore = SSCore.getInstance(getActivity());
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        if (savedInstanceState != null) {
-            Log.v("onCreate from bundle", String.valueOf(position));
-        }
     }
 
     @Override
@@ -101,7 +128,9 @@ public class SSWebViewFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        _view = (ViewGroup) inflater.inflate(R.layout.ss_webview_fragment, null);
+        View _view = inflater.inflate(R.layout.ss_webview_fragment, null);
+
+        _SSPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
 
         _SSWebView = (SSWebView) _view.findViewById(R.id.ss_webview);
         _SSWebViewLoading = (ProgressBar) _view.findViewById(R.id.ss_webview_loading);
@@ -109,108 +138,114 @@ public class SSWebViewFragment extends Fragment {
 
         _SSToolbar = (Toolbar)getActivity().findViewById(R.id.ss_toolbar);
         _SSTabs = (SSSlidingTabLayout) getActivity().findViewById(R.id.ss_tabs);
-        _SSHero = (ImageView) getActivity().findViewById(R.id.ss_hero);
+        _SSHero = (KenBurnsView) getActivity().findViewById(R.id.ss_hero);
         _SSStatusBar = getActivity().findViewById(R.id.ss_status_bar);
 
-        _SSWebView.addJavascriptInterface(new SSWebInterface(getActivity()), "SS");
+        _SSWebView.addJavascriptInterface(new SSWebInterface(getActivity()), "SSBridge");
         _SSWebView.getSettings().setJavaScriptEnabled(true);
         _SSWebView.setWebViewClient(new WebViewClient() {
             public void onPageFinished(WebView view, String url) {
                 _SSWebViewLoading.setVisibility(View.INVISIBLE);
                 view.setVisibility(View.VISIBLE);
+
+                setHighlights(_SSDay._day_highlights);
+                setComments(_SSDay._day_comments);
             }
         });
 
-        _SSWebView.loadDataWithBaseURL(
-                "file:///android_asset/",
-                SSHelper.readFileFromAssets(getActivity(), "index.html").replaceAll(
-                        "<body>",
-                        "<body style='padding-top: "+TOPBAR_PADDING+"px'>"
-                ),
-                "text/html",
-                "utf-8",
-                null
-        );
+        this.loadDay(_SSDayDate);
 
-        _SSWebView.setOnScrollChangedCallback(new SSWebView.OnScrollChangedCallback() {
-            @Override
-            public void onScrollChanged(int deltaX, int deltaY) {
-                int scrollY = _SSWebView.getScrollY() * -1;
-                _SSHero.setTranslationY(scrollY * 0.5f);
-
-                int headerHeight = _SSHero.getHeight() - _SSToolbar.getHeight() - _SSStatusBar.getHeight();
-                float ratio = (float) Math.min(Math.max(scrollY * -1, 0), headerHeight) / headerHeight;
-
-                _SSToolbar.getBackground().setAlpha((int) (ratio * 255));
-                _SSStatusBar.getBackground().setAlpha((int) (ratio * 255));
-                int scrollOtherFragments = scrollY * -1;
-                if (_SSTabs.getTop() + scrollY < _SSToolbar.getHeight() + _SSStatusBar.getHeight()) {
-                    _SSTabs.setY(_SSToolbar.getHeight() + _SSStatusBar.getHeight());
-                    scrollOtherFragments = _SSTabs.getBottom() - (_SSToolbar.getHeight() + _SSStatusBar.getHeight() + _SSTabs.getHeight());
-                } else {
-                    _SSTabs.setTranslationY(scrollY);
-                }
-
-                SparseArray<Fragment> registeredFragments = ((SSMainActivity.SSTabsAdapter) ((SSMainActivity) getActivity()).getPager().getAdapter()).registeredFragments;
-                for (int i = 0; i < registeredFragments.size(); i++) {
-                    if (i == position) continue;
-                    ((SSWebViewFragment) (registeredFragments.get(i)))._SSWebView._SSOnScrollChangedCallbackEnabled = false;
-                    ((SSWebViewFragment) (registeredFragments.get(i)))._SSWebView.scrollTo(0, scrollOtherFragments);
-                    ((SSWebViewFragment) (registeredFragments.get(i)))._SSWebView._SSOnScrollChangedCallbackEnabled = true;
-                }
-            }
-        });
+        _SSWebView.setOnScrollChangedCallback(this);
 
         return _view;
     }
 
+    @Override
+    public void onScrollChanged(int deltaX, int deltaY) {
+        int scrollY = _SSWebView.getScrollY() * -1;
+        _SSHero.setTranslationY(scrollY * 0.5f);
+
+        int headerHeight = _SSHero.getHeight() - _SSToolbar.getHeight() - _SSStatusBar.getHeight();
+        float ratio = (float) Math.min(Math.max(scrollY * -1, 0), headerHeight) / headerHeight;
+
+        ((SSMainActivity)getActivity()).setToolbarStatusBarAlpha((int) (ratio * 255));
+
+        int scrollOtherFragments = scrollY * -1;
+        if (_SSTabs.getTop() + scrollY < _SSToolbar.getHeight() + _SSStatusBar.getHeight()) {
+            _SSTabs.setY(_SSToolbar.getHeight() + _SSStatusBar.getHeight());
+            scrollOtherFragments = _SSTabs.getBottom() - (_SSToolbar.getHeight() + _SSStatusBar.getHeight() + _SSTabs.getHeight());
+        } else {
+            _SSTabs.setTranslationY(scrollY);
+        }
+
+        SparseArray<Fragment> registeredFragments = ((SSTabsAdapter) ((SSMainActivity) getActivity()).getPager().getAdapter()).registeredFragments;
+        for (int i = 0; i < registeredFragments.size(); i++) {
+            if (i == _SSPosition) continue;
+            ((SSWebViewFragment) (registeredFragments.get(i)))._SSWebView.scrollTo(0, scrollOtherFragments);
+        }
+    }
 
     private class SSWebInterface {
         Context _SSContext;
-
         SSWebInterface(Context c) {
             _SSContext = c;
         }
 
         @JavascriptInterface
-        public void highlightStart(final String sRect) {
-            Log.v("RECT", String.valueOf(sRect));
-
-            getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        JSONObject rect = new JSONObject(sRect);
-                        RelativeLayout rl = (RelativeLayout) _view.findViewById(R.id.ss_webview_holder);
-                        ImageView iv = new ImageView(getActivity());
-
-                        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(90, 20);
-                        params.leftMargin = rect.getInt("left");
-                        params.topMargin = rect.getInt("top") + 216;
-                        iv.setBackgroundColor(Color.RED);
-                        rl.addView(iv, params);
-                    } catch (Exception e){
-                        Log.v("E", e.getMessage());
-                    }
-                }
-            });
+        public void openBible(final String verse){
+            try {
+                Intent _SSBibleActivityIntent = new Intent(getActivity().getApplicationContext(), SSBibleVerseActivity.class);
+                _SSBibleActivityIntent.putExtra(SSConstants.SS_BIBLE_VERSE_ACTIVITY_ARGUMENT, _SSDay._day_verses_parsed.getString(verse));
+                startActivity(_SSBibleActivityIntent);
+            } catch (Exception e){}
         }
 
         @JavascriptInterface
-        public void highlightStop(){
-            getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    RelativeLayout rl = (RelativeLayout) _view.findViewById(R.id.ss_webview_holder);
-                    int count = rl.getChildCount();
-                    for (int i = 0; i <= count; i++) {
-                        View v = rl.getChildAt(i);
-                        if (v instanceof ImageView) {
-                            rl.removeView(v);
-                        }
-                    }
-                }
-            });
+        public void saveHighlights(String highlights){
+            try {
+                // TODO: tracking
+                _SSCore.ssSaveHighlights(_SSDay._serial, highlights);
+            } catch (Exception e){}
+        }
+
+        @JavascriptInterface
+        public void saveComments(String comments){
+            try {
+                // TODO: tracking
+                _SSCore.ssSaveComments(_SSDay._serial, comments);
+            } catch (Exception e){}
+        }
+
+        @JavascriptInterface
+        public void copy(String selection){
+            try {
+                // TODO: tracking
+                ClipboardManager _clipboard = (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText(SSConstants.SS_WEBVIEW_CLIPBOARD_LABEL, selection);
+                _clipboard.setPrimaryClip(clip);
+                Toast.makeText(getActivity().getApplicationContext(), getString(R.string.ss_webview_menu_copied), Toast.LENGTH_LONG).show();
+            } catch (Exception e){}
+        }
+
+        @JavascriptInterface
+        public void search(String selection){
+            try {
+                // TODO: tracking
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.format(SSConstants.SS_WEBVIEW_SEARCH_PROVIDER, selection)));
+                startActivity(intent);
+            } catch (Exception e){}
+        }
+
+        @JavascriptInterface
+        public void share(String selection){
+            try {
+                // TODO: tracking
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, selection);
+                sendIntent.setType("text/plain");
+                startActivity(Intent.createChooser(sendIntent, getResources().getText(R.string.ss_webview_menu_share_text_to)));
+            } catch (Exception e){}
         }
     }
 
