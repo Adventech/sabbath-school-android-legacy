f9c28a6087df77cfdbf2a618ae388751d388146b
Vitaliy Lim <lim.vitaliy@gmail.com>
2015-03-18 22:00:27 -0400

    Look & Feel for SSLoadingActivity

--- app/src/main/java/com/cryart/sabbathschool/ui/activity/SSLoadingActivity.java
+++ app/src/main/java/com/cryart/sabbathschool/ui/activity/SSLoadingActivity.java
@@ -29,21 +29,51 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.cryart.sabbathschool.R;
 import com.cryart.sabbathschool.util.SSConstants;
 import com.cryart.sabbathschool.util.SSCore;
+import com.cryart.sabbathschool.util.SSNotification;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Locale;
 
 public class SSLoadingActivity extends Activity {
+    TextView _SSLoadingExplainer;
+    ProgressBar _SSLoadingSpinner;
+    Button _SSLoadingRetry;
+
+    private void setupWidgets(){
+        _SSLoadingSpinner = (ProgressBar) findViewById(R.id.ss_loading_spinner);
+        _SSLoadingExplainer = (TextView) findViewById(R.id.ss_loading_explainer);
+        _SSLoadingRetry = (Button) findViewById(R.id.ss_loading_retry);
+    }
+
+    private void tryLoadAndOpenMainActivity(){
+        _SSLoadingSpinner.setVisibility(View.VISIBLE);
+        _SSLoadingRetry.setVisibility(View.INVISIBLE);
+        _SSLoadingSpinner.animate();
+        _SSLoadingExplainer.setText(getString(R.string.ss_loading));
+        new QuarterlyDownloader(this).execute();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ss_loading_activity);
+        setupWidgets();
+
+        _SSLoadingRetry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tryLoadAndOpenMainActivity();
+            }
+        });
 
         SharedPreferences ssPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         String[] _SSSupportedLanguages = getResources().getStringArray(R.array.ss_settings_lesson_language_values);
@@ -57,11 +87,14 @@ public class SSLoadingActivity extends Activity {
             ssPreferences.edit().putString(SSConstants.SS_SETTINGS_LANGUAGE_KEY, _SSLanguage).commit();
             SSCore.getInstance(this);
             SSCore.refreshLanguage();
+
+            if (SSConstants.SS_SETTINGS_NOTIFICATION_ENABLED_DEFAULT_VALUE){
+                SSNotification.setRepeatingNotification(getApplicationContext());
+            }
         }
 
         PreferenceManager.setDefaultValues(this, R.xml.ss_settings, false);
-
-        new QuarterlyDownloader(this).execute();
+        tryLoadAndOpenMainActivity();
     }
 
     private class QuarterlyDownloader extends AsyncTask<Void, Void, Boolean> {
@@ -72,20 +105,27 @@ public class SSLoadingActivity extends Activity {
         }
 
         @Override
-        protected Boolean doInBackground(Void... params){
+        protected Boolean doInBackground(Void... params) {
             SSCore ss_core = SSCore.getInstance(this.context);
 
             try {
                 ss_core.copyDataBaseIfNeeded();
-            } catch (IOException e){}
+            } catch (IOException e) {}
 
-            return ss_core.downloadIfNeeded();
+            if (ss_core.quarterlyForLanguageExists())
+                return true;
+            else {
+                _SSLoadingExplainer.setText(getString(R.string.ss_downloading));
+                return ss_core.downloadIfNeeded();
+            }
         }
 
         @Override
         protected void onPostExecute(Boolean result) {
             if (!result) {
-                Log.v("Error downloading", "Error downloading");
+                _SSLoadingSpinner.setVisibility(View.INVISIBLE);
+                _SSLoadingRetry.setVisibility(View.VISIBLE);
+                _SSLoadingExplainer.setText(getString(R.string.ss_downloading_error));
             } else {
                 Intent i = new Intent(getApplicationContext(), SSMainActivity.class);
                 startActivity(i);
